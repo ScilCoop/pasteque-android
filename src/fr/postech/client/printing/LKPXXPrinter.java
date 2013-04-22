3f638d7a06282226cd61b3e471d15bb459f8d489
Cédric Houbart <cedric@scil.coop>
2013-04-22 17:22:19 +0200

    Use Sewoo library for printing (doesn't work on all devices)

--- src/fr/postech/client/printing/LKPXXPrinter.java
+++ src/fr/postech/client/printing/LKPXXPrinter.java
@@ -25,6 +25,12 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.content.Context;
+import android.os.AsyncTask;
+import com.sewoo.jpos.command.ESCPOS;
+import com.sewoo.jpos.command.ESCPOSConst;
+import com.sewoo.jpos.printer.ESCPOSPrinter;
+import com.sewoo.port.android.BluetoothPort;
+import com.sewoo.request.android.RequestHandler;
 import java.io.InputStream;
 import java.io.IOException;
 import java.io.OutputStream;
@@ -36,73 +42,65 @@ import java.util.Date;
 
 public class LKPXXPrinter implements Printer {
 
-    private static String CUT = "27|#fP";
-    private static String LF = "\n";//"10";
+    private static final char ESC = ESCPOS.ESC;
+    private static final char LF = ESCPOS.LF;
+    private static final String CUT = ESC + "|#fP";
 
     private String address;
     private Context ctx;
+    private ESCPOSPrinter printer;
     private BluetoothSocket sock;
-    private OutputStream stream;
+    private BluetoothPort port;
+    private Thread hThread;
 
     public LKPXXPrinter(Context ctx, String address) {
         this.address = address;
         this.ctx = ctx;
+        this.port = BluetoothPort.getInstance();
+        this.printer = new ESCPOSPrinter();
     }
 
     public void connect() throws IOException {
         BluetoothAdapter btadapt = BluetoothAdapter.getDefaultAdapter();
         BluetoothDevice dev = btadapt.getRemoteDevice(this.address);
-        try {
-            Method m = dev.getClass().getMethod("createRfcommSocket",
-                    new Class[] {int.class});
-            if (this.sock == null) {
-                this.sock = (BluetoothSocket)m.invoke(dev,
-                        Integer.valueOf(1));
-            }
-            this.sock.connect();
-            if (this.stream == null) {
-                this.stream = sock.getOutputStream();
-            }
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace();
-        } catch (InvocationTargetException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        }
+        new ConnTask().execute(dev);
     }
 
     public void disconnect() throws IOException {
-        if (this.sock != null) {
-            this.sock.close();
-            this.sock = null;
-            this.stream = null;
+        try {
+            this.port.disconnect();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        if ((hThread != null) && (hThread.isAlive())) {
+            hThread.interrupt();
         }
     }
 
-    private void print(String data) throws IOException {
-        if (this.stream != null) {
-            String ascii = data.replace("é", "e");
-            ascii = ascii.replace("è", "e");
-            ascii = ascii.replace("ê", "e");
-            ascii = ascii.replace("ë", "e");
-            ascii = ascii.replace("à", "a");
-            ascii = ascii.replace("ï", "i");
-            ascii = ascii.replace("ô", "o");
-            ascii = ascii.replace("ç", "c");
-            ascii = ascii.replace("ù", "u");
-            ascii = ascii.replace("É", "E");
-            ascii = ascii.replace("È", "E");
-            ascii = ascii.replace("Ê", "E");
-            ascii = ascii.replace("Ë", "E");
-            ascii = ascii.replace("À", "A");
-            ascii = ascii.replace("Ï", "I");
-            ascii = ascii.replace("Ô", "O");
-            ascii = ascii.replace("Ç", "c");
-            ascii = ascii.replace("Ù", "u");
-            this.stream.write(ascii.getBytes("ASCII"));
-            this.stream.flush();
-        }
+    private void printLine(String data) throws IOException {
+        String ascii = data.replace("é", "e");
+        ascii = ascii.replace("è", "e");
+        ascii = ascii.replace("ê", "e");
+        ascii = ascii.replace("ë", "e");
+        ascii = ascii.replace("à", "a");
+        ascii = ascii.replace("ï", "i");
+        ascii = ascii.replace("ô", "o");
+        ascii = ascii.replace("ç", "c");
+        ascii = ascii.replace("ù", "u");
+        ascii = ascii.replace("É", "E");
+        ascii = ascii.replace("È", "E");
+        ascii = ascii.replace("Ê", "E");
+        ascii = ascii.replace("Ë", "E");
+        ascii = ascii.replace("À", "A");
+        ascii = ascii.replace("Ï", "I");
+        ascii = ascii.replace("Ô", "O");
+        ascii = ascii.replace("Ç", "c");
+        ascii = ascii.replace("Ù", "u");
+        this.printer.printNormal(ascii + LF);
+    }
+
+    private void printLine() {
+        this.printer.lineFeed(1);
     }
 
     public void printReceipt(Receipt r) throws IOException {
@@ -110,38 +108,37 @@ public class LKPXXPrinter implements Printer {
         // Title
         DateFormat df = DateFormat.getDateTimeInstance();
         String date = df.format(new Date(r.getPaymentTime() * 1000));
-        this.print(padAfter("Date : ", 7) + padBefore(date, 25) + LF);
-        this.print(LF);
+        this.printLine(padAfter("Date : ", 7) + padBefore(date, 25));
+        this.printLine();
         // Content
-        this.print(padAfter("Article", 10));
-        this.print(padBefore("Prix", 7));
-        this.print(padBefore("", 5));
-        this.print(padBefore("Total", 10) + LF);
-        this.print(LF);
-        this.print("--------------------------------" + LF);
+        this.printLine(padAfter("Article", 10) + padBefore("Prix", 7)
+                + padBefore("", 5) + padBefore("Total", 10));
+        this.printLine();
+        this.printLine("--------------------------------");
         for (TicketLine line : r.getTicket().getLines()) {
-            this.print(padAfter(line.getProduct().getLabel(), 32));
-            this.print(padBefore(priceFormat.format(line.getProduct().getTaxedPrice()), 17));
-            this.print(padBefore("x" + line.getQuantity(), 5));
-            this.print(padBefore(priceFormat.format(line.getTotalPrice()), 10));
-            this.print(LF);
+            this.printLine(padAfter(line.getProduct().getLabel(), 32)
+                    + padBefore(priceFormat.format(line.getProduct().getTaxedPrice()), 17)
+                    + padBefore("x" + line.getQuantity(), 5)
+                    + padBefore(priceFormat.format(line.getTotalPrice()), 10));
         }
-        this.print("--------------------------------" + LF);
+        this.printLine("--------------------------------");
         // Taxes
         // Total
-        this.print(padAfter("Total", 15));
-        this.print(padBefore(priceFormat.format(r.getTicket().getTotalPrice()), 17));
-        this.print(padAfter("Dont TVA", 15));
-        this.print(padBefore(priceFormat.format(r.getTicket().getTaxPrice()), 17));
+        this.printLine(padAfter("Total", 15)
+                + padBefore(priceFormat.format(r.getTicket().getTotalPrice()), 17));
+        this.printLine(padAfter("Dont TVA", 15)
+                + padBefore(priceFormat.format(r.getTicket().getTaxPrice()), 17));
         // Payments
-        this.print(LF + LF);
+        this.printLine();
+        this.printLine();
         for (Payment pmt : r.getPayments()) {
-            this.print(padAfter(pmt.getMode().getLabel(this.ctx), 18));
-            this.print(padBefore(priceFormat.format(pmt.getAmount()), 32));
-            this.print(LF);
+            this.printLine(padAfter(pmt.getMode().getLabel(this.ctx), 18)
+                    + padBefore(priceFormat.format(pmt.getAmount()), 32));
         }
         // Cut
-        this.print(LF + LF + LF);
+        this.printLine();
+        this.printLine();
+        this.printLine();
     }
 
     private static String padBefore(String text, int size) {
@@ -160,4 +157,45 @@ public class LKPXXPrinter implements Printer {
         }
         return ret;
     }
+
+	// Bluetooth Connection Task.
+	class ConnTask extends AsyncTask<BluetoothDevice, Void, Integer> {
+		
+		@Override
+		protected void onPreExecute()
+		{
+			super.onPreExecute();
+		}
+		
+		@Override
+		protected Integer doInBackground(BluetoothDevice... params)
+		{
+			Integer retVal = null;
+			try
+			{
+				port.connect(params[0]);
+				retVal = new Integer(0);
+			}
+			catch (IOException e) {
+			e.printStackTrace();
+				retVal = new Integer(-1);
+			}
+			return retVal;
+		}
+		
+		@Override
+		protected void onPostExecute(Integer result)
+		{
+			if(result.intValue() == 0)	// Connection success.
+			{
+				RequestHandler rh = new RequestHandler();				
+				hThread = new Thread(rh);
+				hThread.start();
+			}
+			else	// Connection failed.
+			{
+			}
+			super.onPostExecute(result);
+		}
+	}
 }
