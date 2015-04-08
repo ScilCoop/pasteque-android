afc3af0c9d848391c7db9b903fd5bfccac97d667
Philippe Pary <philippe@scil.coop>
2015-04-08 15:10:47 +0200

    humpf

--- libs/lib-appcompat/libs/lib-appcompat/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/philippe/code/pasteque-android/libs/lib-appcompat/libs/lib-appcompat/bin/AndroidManifest.xml : \
-/home/philippe/code/pasteque-android/libs/lib-appcompat/libs/lib-appcompat/AndroidManifest.xml \
