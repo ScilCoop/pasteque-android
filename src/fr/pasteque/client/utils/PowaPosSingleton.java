eb6ed7d4ce80fe96290cbe02e1722bef1338e125
Pelayo PAREJO PAGADOR <pelayo.parejopagador@epitech.eu>
2015-05-26 18:02:50 +0200

    PowaPosSingleton updated for new sdk

--- src/fr/pasteque/client/utils/PowaPosSingleton.java
+++ src/fr/pasteque/client/utils/PowaPosSingleton.java
@@ -12,38 +12,30 @@ import com.mpowa.android.sdk.powapos.core.callbacks.PowaPOSCallback;
  *  Singleton to prevent multiple connection to Powa
  *  PowaPos sdk 1.14.0 has one built-in
  *  This one is thread safe thought
+ *
+ *  Will be useful in the future
  *  WIP
  */
-public class PowaPosSingleton extends PowaPOS {
+public class PowaPosSingleton extends PowaPOSSingleton {
     private static final String POWA_POS_SING_TAG = "PowaPosSingleton";
     private static boolean mCreated = false;
     private static PowaPosSingleton mInstance;
-    private static Context mContext;
-    private static PowaPOSCallback mCallback;
 
     // Protected to for use of getInstance;
-    protected PowaPosSingleton(Context context, PowaPOSCallback callback) {
-        super(context, callback);
+    protected PowaPosSingleton() {
+        super();
     }
 
     public synchronized static PowaPosSingleton getInstance() {
         if (mInstance == null) {
-            mInstance = new PowaPosSingleton(null, null);
+            mInstance = new PowaPosSingleton();
         }
         return mInstance;
     }
 
     public synchronized void create(Context context, PowaPOSCallback callback) {
-        if (context == null) {
-            throw new RuntimeException(POWA_POS_SING_TAG
-                    + ".create()'s context should not be null");
-        }
-        if (!mCreated) {
-            mCreated = true;
-            this.peripheralExternalEvents = callback;
-        	//TODO : does not compile
-            //PowaPeripheralCallbackIntMgr.getInstance().addListener(this.peripheralInternalEvents);
-        }
+        super.create(context, callback);
+        mCreated = true;
     }
 
     public synchronized void dispose() {
