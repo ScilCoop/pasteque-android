449463b7c75e964029d654ee88dfd89a54b8c682
Nicolas Svirchevsky <n.svirchevsky@gmail.com>
2015-06-15 15:43:29 +0100

    simple test (bis)

--- tests/src/fr/pasteque/client/StartTest.java
+++ tests/src/fr/pasteque/client/StartTest.java
@@ -20,16 +20,6 @@ package fr.pasteque.client;
 
 import android.test.ActivityInstrumentationTestCase2;
 
-/**
- * This is a simple framework for a test of an Application.  See
- * {@link android.test.ApplicationTestCase ApplicationTestCase} for more information on
- * how to write and extend Application tests.
- * <p/>
- * To run this test, you can type:
- * adb shell am instrument -w \
- * -e class fr.pasteque.client.StartTest \
- * fr.pasteque.client.tests/android.test.InstrumentationTestRunner
- */
 public class StartTest extends ActivityInstrumentationTestCase2<Start> {
 
     private Start activity;
