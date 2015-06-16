ee49c0e1ec65db7f22e4e8f7cfab80bda937d28d
Nicolas Svirchevsky <n.svirchevsky@gmail.com>
2015-06-16 16:26:20 +0100

    Test environment improved. New/improved Target:   - clean   - debug (compile test.apk)   - installd (install test.apk)   - test (launch test installed on the device)   - compile (compile unit test)   - junit (test unit test)

--- tests/src/fr/pasteque/client/test/StartTest.java
+++ tests/src/fr/pasteque/client/test/StartTest.java
@@ -16,8 +16,9 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-package fr.pasteque.client;
+package fr.pasteque.client.test;
 
+import fr.pasteque.client.Start;
 import android.test.ActivityInstrumentationTestCase2;
 
 public class StartTest extends ActivityInstrumentationTestCase2<Start> {
---
+++ tests/src/fr/pasteque/client/unit/models/CashTest.java
@@ -0,0 +1,25 @@
+
+package fr.pasteque.client.unit.models;
+
+import fr.pasteque.client.models.Cash;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class CashTest {
+    
+    @Test
+    public void cashTest() {
+        assertNotNull(new Cash(1));
+    }
+    
+    // ---Pour permettre l'exécution des test----------------------                                                                                                                                                  
+    public static void main(String[] args)
+    {
+        junit.textui.TestRunner.run(suite());
+    }
+    
+    public static junit.framework.Test suite() {
+	return new junit.framework.JUnit4TestAdapter(CashTest.class);
+    }
+
+}
