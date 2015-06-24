b3e788e9d23db038fed82929c1f814b630785a0c
Nicolas Svirchevsky <n.svirchevsky@gmail.com>
2015-06-24 12:13:36 +0100

    barcode test

---
+++ src/fr/pasteque/client/test/BarcodeGeneratorTest.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package fr.pasteque.client.test;
+
+
+import fr.pasteque.client.R;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.Toast;
+import com.google.zxing.common.BitMatrix;
+import fr.pasteque.client.models.Barcode;
+import fr.pasteque.client.utils.BarcodeGenerator;
+
+
+/**
+ *
+ * @author svirch_n
+ */
+public class BarcodeGeneratorTest extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.barcode_generator_test);
+        ImageView image =  (ImageView) findViewById(R.id.image_view);
+        
+        BitMatrix matrix = BarcodeGenerator.generate("7501054530107", Barcode.QR);
+        if (matrix == null)
+            Toast.makeText(this, "matrix null", Toast.LENGTH_SHORT).show();
+        
+        Bitmap bitmap = BarcodeGenerator.toBitmap(matrix);
+        image.setImageBitmap(bitmap);
+    }
+    
+}
