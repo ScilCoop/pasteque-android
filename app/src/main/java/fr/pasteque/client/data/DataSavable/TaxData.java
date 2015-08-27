af5596f9f42b1ad9101f8ac12ffc234844215a58
Nicolas Svirchevsky <n.svirchevsky@gmail.com>
2015-08-27 17:24:34 +0100

    Tax is implemented to manualInput

---
+++ app/src/main/java/fr/pasteque/client/data/DataSavable/TaxData.java
@@ -0,0 +1,53 @@
+package fr.pasteque.client.data.DataSavable;
+
+import fr.pasteque.client.models.Tax;
+import fr.pasteque.client.utils.exception.DataCorruptedException;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by nsvir on 27/08/15.
+ * n.svirchevsky@gmail.com
+ */
+public class TaxData extends AbstractDataSavable {
+
+    private final static String FILENAME = "taxes.data";
+
+    public ArrayList<Tax> taxes = new ArrayList<>();
+
+    @Override
+    protected String getFileName() {
+        return FILENAME;
+    }
+
+    @Override
+    protected List<Object> getObjectList() {
+        List<Object> result = new ArrayList<>();
+        result.add(taxes);
+        return result;
+    }
+
+    @Override
+    protected int getNumberOfObjects() {
+        return 1;
+    }
+
+    @Override
+    protected void recoverObjects(List<Object> objs) throws DataCorruptedException {
+        this.taxes = (ArrayList<Tax>) objs.get(0);
+    }
+
+    public void setTaxes(HashMap<String, Double> taxes) {
+        Map<String, Double> list = taxes;
+        for (Map.Entry<String, Double> entry: list.entrySet()) {
+            this.taxes.add(new Tax(entry.getKey(), entry.getValue()));
+        }
+    }
+
+    public ArrayList<Tax> getTaxes() {
+        return this.taxes;
+    }
+}
