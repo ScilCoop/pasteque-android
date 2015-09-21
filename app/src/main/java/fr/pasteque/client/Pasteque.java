f5584cc31865cceeb4a025897b9e9e793a3735f5
Nicolas Svirchevsky <n.svirchevsky@gmail.com>
2015-09-21 15:37:26 +0100

    Context is a static member of Pasteque which is an Application

---
+++ app/src/main/java/fr/pasteque/client/Pasteque.java
@@ -0,0 +1,23 @@
+package fr.pasteque.client;
+
+import android.app.Application;
+import android.content.Context;
+
+/**
+ * Created by nsvir on 21/09/15.
+ * n.svirchevsky@gmail.com
+ */
+public class Pasteque extends Application {
+
+    private static Context context;
+
+    public void onCreate() {
+        super.onCreate();
+        Pasteque.context = getApplicationContext();
+    }
+
+    public static Context getAppContext() {
+        return Pasteque.context;
+    }
+
+}
